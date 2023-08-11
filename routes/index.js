bc1f7698243d1728cc2218f5d839e7d2164d872c
MuhammadUbaid123 <ubaid_nawaz@elexoft.com>
2023-08-11 11:03:50 +0500

    Reading specific worksheet

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,12 @@ router.get('/', function(req, res, next) {
 
   // Convert worksheet to JSON format
   const data = XLSX.utils.sheet_to_json(worksheet);
+
+  // Update cell values
+  worksheet['B4'].v = '4';
+  worksheet['B7'].v = '8';
+  // Save the changes to the file
+  XLSX.writeFile(workbook, './updated-file.xlsx');
   console.log(data);
     res.render('index', { data: data });
   });
